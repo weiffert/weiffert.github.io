a73a4bda98ff91249af916ec2fa9542f3f35ddc7
weiffert <weiffert@gmail.com>
2018-05-27 13:08:20 -0400

    Create boilerplate structure

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+  render() {
+    return (
+      <div class="Main">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    );
+  }
+}
