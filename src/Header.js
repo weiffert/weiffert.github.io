cb3b962c36a9698cf7594bd1c8431f4154db7e44
weiffert <weiffert@gmail.com>
2018-07-18 19:30:03 -0500

    Create working NavLinks

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 class Header extends Component {
   render() {
-    return <div className="Header">Header</div>;
+    return (
+      <div className="Header">
+        <NavLink to="/project">work</NavLink>
+        <NavLink to="/">William Eiffert</NavLink>
+        <NavLink to="/about">about</NavLink>
+      </div>
+    );
   }
 }
 
