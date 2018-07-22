febc35cb5d49afb7b0dd9c7c363ca61fca766ce8
weiffert <weiffert@gmail.com>
2018-07-22 16:36:31 -0500

    Store github data to localstorage to prevent excess fetches

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -9,14 +9,21 @@ class ProjectList extends Component {
     this.state = {
       repos: [],
     };
-    fetch("https://api.github.com/users/weiffert/repos")
-      .then(response => response.json())
-      .then(repos => {
-        repos.sort((a, b) => (a.pushed_at > b.pushed_at ? -1 : 1));
-        this.setState({
-          repos,
+    if (false) {
+      fetch("https://api.github.com/users/weiffert/repos")
+        .then(response => response.json())
+        .then(repos => {
+          repos.sort((a, b) => (a.pushed_at > b.pushed_at ? -1 : 1));
+          window.localStorage.setItem("repos", JSON.stringify(repos));
+          this.setState({
+            repos,
+          });
         });
-      });
+    } else {
+      const repos = JSON.parse(window.localStorage.getItem("repos"));
+      this.state.repos = repos;
+      console.table(repos);
+    }
   }
 
   render() {
