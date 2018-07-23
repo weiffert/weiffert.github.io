cb36631ceff27ddc8c6bb0693a83509a9bd6b261
weiffert <weiffert@gmail.com>
2018-07-22 20:30:30 -0500

    Decode and parse README from Github

--- src/Project.js
+++ src/Project.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import Base64 from "base-64";
+import Commonmark from "commonmark";
 import "./Project.css";
 
 class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      body: "BAD wifi. PLEASE bear WITH me.",
+    };
+    fetch(`${this.props.repo.url}/readme`)
+      .then(response => response.json())
+      .then(response => {
+        const markdown = Base64.decode(response.content);
+        this.setState({ body: markdown });
+      });
+  }
   render() {
     console.log(this.props);
-    return <div className="Project">Project {this.props.repo.name}</div>;
+    const reader = new Commonmark.Parser();
+    const render = new Commonmark.HtmlRenderer();
+    const parsed = reader.parse(this.state.body);
+    const html = render.render(parsed);
+    return (
+      <div className="Project" dangerouslySetInnerHTML={{ __html: html }} />
+    );
   }
 }
 
