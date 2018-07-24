70fd7119c738457bfc49e5b37c864b0a79691bf8
weiffert <weiffert@gmail.com>
2018-07-23 21:07:36 -0500

    Show contact info

--- src/Splash.js
+++ src/Splash.js
@@ -10,9 +10,26 @@ class Splash extends Component {
           <h1>William Eiffert</h1>
         </NavLink>
         <div className="links">
-          <NavLink to="/project">work</NavLink>
-          <NavLink to="/about">about</NavLink>
-          <NavLink to="/contact">contact</NavLink>
+          <NavLink to="/project">
+            <p>work</p>
+          </NavLink>
+          <NavLink to="/about">
+            <p>about</p>
+          </NavLink>
+          <p>
+            <a target="_blank" href="mailto:weiffert@purdue.edu">
+              weiffert@purdue.edu
+            </a>
+            <a target="_blank" href="https://github.com/weiffert">
+              github
+            </a>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/william-eiffert/"
+            >
+              linkedin
+            </a>
+          </p>
         </div>
       </div>
     );
