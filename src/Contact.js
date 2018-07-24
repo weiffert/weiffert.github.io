2c5869b1c6d7e87e47a29290d8f53263e33e1009
weiffert <weiffert@gmail.com>
2018-07-23 21:26:53 -0500

    Basic Contact structure

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,37 @@ import "./Contact.css";
 
 class Contact extends Component {
   render() {
-    return <div className="Contact">Contact</div>;
+    return (
+      <div className="Contact">
+        <h1>get in touch</h1>
+        <a target="_blank" href="mailto:weiffert@purdue.edu">
+          weiffert@purdue.edu
+        </a>
+        {/* <form>
+          <input
+            type="text"
+            id="text"
+            name="text"
+            placeholder="drop me a line..."
+            required
+          />
+          <input
+            type="name"
+            id="name"
+            name="name"
+            placeholder="name"
+            required
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="email"
+            required
+          />
+        </form> */}
+      </div>
+    );
   }
 }
 
