0805586188602fb518c818fad6ab1dc91139294d
weiffert <weiffert@gmail.com>
2018-07-23 20:35:56 -0500

    Basic structure for Splash

--- src/Splash.js
+++ src/Splash.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 import "./Splash.css";
 
 class Splash extends Component {
   render() {
-    return <div className="Splash">Splash</div>;
+    return (
+      <div className="Splash">
+        <NavLink to="/">
+          <h1>William Eiffert</h1>
+        </NavLink>
+        <div className="links">
+          <NavLink to="/project">work</NavLink>
+          <NavLink to="/about">about</NavLink>
+          <NavLink to="/contact">contact</NavLink>
+        </div>
+      </div>
+    );
   }
 }
 
