86634a53d2ea89a1e92127949d4cfb711a08dc35
weiffert <weiffert@gmail.com>
2018-08-10 20:12:42 -0700

    Move header to work

--- src/Work.js
+++ src/Work.js
@@ -3,11 +3,13 @@ import "./Work.css";
 
 import Splash from "./Splash";
 import ProjectList from "./ProjectList";
+import Header from "./Header";
 
 class Work extends Component {
   render() {
     return (
       <div className="Work">
+        <Header scrollY={this.props.scrollY} />
         <Splash />
         <ProjectList max={this.props.repos.length} repos={this.props.repos} />
       </div>
