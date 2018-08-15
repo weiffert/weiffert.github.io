a64e022d10589e635a25ab029d5bf9ce51277f00
weiffert <weiffert@gmail.com>
2018-08-15 13:52:42 -0500

    Simpler coloring of projectItems

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -4,16 +4,6 @@ import "./ProjectList.css";
 import ProjectItem from "./ProjectItem";
 
 class ProjectList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      colors: [],
-    };
-    this.props.repos.forEach((repo, index) => {
-      this.state.colors.push(this.getColor(index + 1, this.props.repos.length));
-    });
-  }
-
   getColor = (index, total) => {
     const multiplier = (index / total) * 255;
     return `rgb(${multiplier}, ${multiplier}, ${multiplier})`;
@@ -26,7 +16,7 @@ class ProjectList extends Component {
           (repo, index) =>
             index < this.props.max ? (
               <ProjectItem
-                color={this.state.colors[index]}
+                color={this.getColor(index + 1, this.props.repos.length)}
                 project={repo}
                 key={index}
                 fontColor={index < this.props.max / 2 ? "white" : "black"}
